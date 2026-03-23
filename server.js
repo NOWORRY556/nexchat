65a1859363676da629096c8a200f58d661808576
Krishna_dev <krishnasaraf9d@gmail.com>
2026-03-23 22:19:35 +0530

    fix port for render

--- server.js
+++ server.js
@@ -79,6 +79,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('NexChat running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('NexChat running on port ' + PORT);
 });
